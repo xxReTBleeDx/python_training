3d869a9ce6faeee405cf7466c48270d15577859c
ReTBleeD <igavikrov@proton.me>
2024-07-18 19:47:20 -0400

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -3,32 +3,29 @@ import re
 
 def validate_password(password):
     if len(password) < 8:
-        return "Le mot de passe doit contenir au moins 8 characteres"
+        return "Le mot de passe doit contenir au moins un chiffre"
     
-    if not re.search(r'\d', password):
-        return "Le mot de passe doit contenir au moins un digits"
-
-
-    if not re.search(f'[A-Z]', password):
+    if not re.search(r'[A-Z]', password):
         return "Le mot de passe doit contenir au moins une majuscule"
     
-    if not re.search(f'[~!@#$%^&*()_+=~;:?/.,<>]'):
-        return "Le mot de passe doit contenir au moins un character"
+    if not re.search(r'[a-z]', password):
+        return "Le mot de passe doit contnir au moins une minuscule"
     
-    else:
-        return "Bravo vous avez entre le bon mot de passe"
+    if not re.search(r'[~!@#$%^&*()_+={}\[\]:;"\'<>,.?/\\|`]', password):
+        return "Le mot de passe doit contneir au moins un caractere speciale"
+    
 
+    return "Bravo vous avez entre le bon mot de passe"
 
 
 def demander():
     password = input("Entrez un mot de passe: ")
     return password
 
-
 def main():
     password = demander()
     validate_result = validate_password(password)
     print(validate_result)
 
-if __name__=="__main__":
+if __name__=='__main__':
     main()
