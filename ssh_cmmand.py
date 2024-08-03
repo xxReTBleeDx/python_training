83720fcbdb13f32e6a6c2239498a8d7707814763
ReTBleeD <igavikrov@proton.me>
2024-08-03 17:06:32 -0400

    ssh_cmmand.py

--- ssh_cmmand.py
+++ ssh_cmmand.py
@@ -2,19 +2,20 @@ import paramiko
 
 def ssh_command(ip, port, user, passwd, cmd):
   client = paramiko.SSHClient()
-  client.connect(ip, port, username=user, password=passwd)
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  client.connect(ip, port=port, username=user, password=passwd)
   _, stdout, stderr = client.exec_command(cmd)
-  output = stdout.readlines() + stderr.readlines()
+  output = stdout.readlines() + stderr.readliens()
   if output:
-    print('--Output--')
+    print('---Output---')
     for line in output:
       print(line.strip())
     client.close()
 
 if __name__=='__main__':
   import getpass
-  user = input('Username: ')
-  password = getpass.getpass()
-  ip = input('Enter server IP: ') or 2222
-  ssh_command(ip, port,  user, password, cmd)
-  
+  user = input('Enter your username: > ')
+  password = getpass.getpass('Enter your passowrd: > ')
+  ip = input('Enter server IP: or <CR> ') or '192.168.1.203'
+  port = input('Enter PORT: or <CR> ') or 2222
+  cmd = input('Enter command: or <CR>: ') or 'id'
