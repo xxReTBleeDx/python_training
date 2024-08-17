1e0f6d79d38f074172ae7e425ee1e5c11c947318
ReTBleeD <igavikrov@proton.me>
2024-08-17 18:26:40 -0400

    Update

---
+++ packet_sniffer.py
@@ -0,0 +1,69 @@
+import ipaddress
+import os
+import socket
+import struct
+import sys
+
+class IP:
+    def __init__(self, buff=None):
+        header = struct.unpack('<BBHHHBBH4s4s', buff)
+        self.ver = header[0] >> 4
+        self.ihl = header[0] & 0xF
+        self.tos = header[1]
+        self.len = header[2]
+        self.id = header[3]
+        self.offset = header[4]
+        self.ttl = header[5]
+        self.protocol_num = header[6]
+        self.sum = header[7]
+        self.src = header[8]
+        self.dst = header[9]
+
+        # Convertir les adresses IP en format lisible
+        self.src_address = ipaddress.ip_address(self.src)
+        self.dst_address = ipaddress.ip_address(self.dst)
+
+        # Mappage des numéros de protocole à leurs noms
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except KeyError:
+            self.protocol = str(self.protocol_num)
+
+def sniff(host):
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 0))
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    try:
+        while True:
+            # Lire un paquet
+            raw_buffer = sniffer.recvfrom(65535)[0]
+
+            # Créer un en-tête IP à partir des 20 premiers octets
+            ip_header = IP(raw_buffer[0:20])
+
+            # Afficher le protocole détecté et les hôtes
+            print('Protocol: %s %s -> %s' % (ip_header.protocol,
+                                             ip_header.src_address,
+                                             ip_header.dst_address))
+    except KeyboardInterrupt:
+        # Si on est sous Windows, désactiver le mode promiscuous
+        if os.name == 'nt':
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        sys.exit()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = '192.168.1.203'
+    sniff(host)
